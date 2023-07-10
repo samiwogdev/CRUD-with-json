e2c846e58950f9be1a2ad1beafd09d2120372f71
Samuel Idowu Ogunbodede <samiwogdev@gmail.com>
2023-07-10 10:07:24 +0100

    users.php and users.son file created

---
+++ users.php
@@ -0,0 +1,22 @@
+<?php
+
+function getUsers(){
+    
+}
+
+function getUserById(){
+    
+}
+
+function createUser(){
+    
+}
+
+function updateUsers(){
+    
+}
+
+function deleteUsers(){
+    
+}
+
