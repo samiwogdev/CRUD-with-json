23ff1f3e8fe02651b25950e15ed2b1da85ba31e3
Samuel Idowu Ogunbodede <samiwogdev@gmail.com>
2023-07-10 11:09:42 +0100

    index page updated
    index page updated to fetch data form user.json

--- index.php
+++ index.php
@@ -1,7 +1,49 @@
 <?php
+require_once 'users.php';
+$users = getUsers();
+?>
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+              integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
+        <title>Simple PHP CRUD</title>
+    </head>
+    <body>
+        <table class="table">
+            <thead>
+                <tr>
+<!--                    <th>Image</th>-->
+                    <th>Name</th>
+                    <th>Username</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>Website</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($users as $user): ?>
+                    <tr>
+                        <td><?php echo $user['name'] ?></td>
+                        <td><?php echo $user['username'] ?></td>
+                        <td><?php echo $user['email'] ?></td>
+                        <td><?php echo $user['phone'] ?></td>
+                        <td><?php echo $user['website'] ?></td>
+                        <td> 
+                            <a href="view.php?id=<?php echo $user['id'] ?>" class="btn btn-sm btn-outline-info">View</a>
+                            <a href="update.php?id=<?php echo $user['id'] ?>" class="btn btn-sm btn-outline-secondary">Update</a>
+                            <a href="delete.php?id=<?php echo $user['id'] ?>" class="btn btn-sm btn-outline-danger">Delete</a>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </body>
+</html>
--- users.php
+++ users.php
@@ -1,7 +1,8 @@
 <?php
 
 function getUsers(){
-    
+   return  json_decode(file_get_contents(__DIR__.'/users.json'), true);  //for array of object
+//   return  json_decode(file_get_contents(__DIR__.'/users.json'), true);  //for array of array
 }
 
 function getUserById(){
